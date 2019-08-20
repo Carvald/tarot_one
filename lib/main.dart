141d713fd680e19724ece0d4819cb85ba8bac169
carvald <cvalderrama@tecnosoftware.com>
2019-08-20 11:20:32 -0300

    avance firebase

--- lib/main.dart
+++ lib/main.dart
@@ -6,30 +6,61 @@ import 'package:redux_thunk/redux_thunk.dart';
 import 'screens/login/login_screen.dart';
 import 'model/redux/state/app_state.dart';
 import 'model/navigation/navigation.dart';
-import 'screens/home//home_screen.dart';
-import 'screens/dealt//dealt_screen.dart';
+import 'screens/home/home_screen.dart';
+import 'screens/dealt/dealt_screen.dart';
 import 'model/redux/reducers/app_reducer.dart';
+import 'package:provider/provider.dart';
+import 'package:tarot_one/model/redux/actions/services/login/auth_service.dart';
+import 'package:tarot_one/middleware/auth_middleware.dart';
 
-void main() => runApp(MyApp());
+
+//void main() => runApp(MyApp());
+void main() =>runApp(ChangeNotifierProvider<AuthService>(
+        child: MyApp(),
+        builder: (BuildContext context) {
+          return AuthService();
+        },
+      ),);
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   final DevToolsStore<AppState> store = DevToolsStore<AppState>(
       appReducer,
       initialState: AppState.initial(),
-      middleware: [thunkMiddleware],
+      //middleware: [thunkMiddleware],
+      middleware: []
+      ..addAll(createAuthMiddleware())
+      ..add(thunkMiddleware),
     );
   @override
   Widget build(BuildContext context) {
      return StoreProvider(
       store: store,
       child: MaterialApp(
-        title: 'Flutter Redux Example',
+        title: 'Flutter TC',
         navigatorKey: Keys.navKey,
         theme: ThemeData(
           primarySwatch: Colors.deepPurple,
         ),
-        home: LoginScreen(title: 'Log in'),
+        home: /*FutureBuilder<FirebaseUser>(
+        future: Provider.of<AuthService>(context).getUser(),
+        builder: (context, AsyncSnapshot<FirebaseUser> snapshot) { //          ⇐ NEW
+          if (snapshot.connectionState == ConnectionState.done) {
+            // log error to console                                            ⇐ NEW
+            if (snapshot.error != null) {
+              print("error");
+              return Text(snapshot.error.toString());
+            }
+            // redirect to the proper page, pass the user into the 
+            // `HomePage` so we can display the user email in welcome msg     ⇐ NEW
+            return snapshot.hasData ? HomeScreen() : LoginScreen(title: 'Log in');//Loginpage()
+          } else {
+            // show loading indicator                                         ⇐ NEW
+            return LoadingCircle();
+          }
+        },
+      ),*/
+      LoginScreen(title: 'Log in'),
         routes: {
           Routes.homeScreen: (context) {
             return HomeScreen(title:'Home Screen');
@@ -41,5 +72,19 @@ class MyApp extends StatelessWidget {
       ),
     );
     
+  }
+
+  
+}
+
+class LoadingCircle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: CircularProgressIndicator(),
+        alignment: Alignment(0.0, 0.0),
+      ),
+    );
   }
 }
--- lib/model/redux/state/app_state.dart
+++ lib/model/redux/state/app_state.dart
@@ -1,13 +1,15 @@
 import 'package:meta/meta.dart';
 import 'package:tarot_one/model/redux/state/user/user_state.dart';
 import 'package:tarot_one/model/redux/state/cards/cards_state.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 @immutable
 class AppState {
   final UserState userState;
   final DealState cardsState;
+  final FirebaseUser currentUser; 
 
-  AppState({@required this.userState,@required this.cardsState});
+  AppState({@required this.userState,@required this.cardsState,@required this.currentUser});
 
   factory AppState.initial() {
     return AppState(
@@ -19,19 +21,32 @@ class AppState {
   AppState copyWith({
     UserState userState,
     DealState cardsState,
+    FirebaseUser currentUser,
   }) {
     return AppState(
       userState: userState ?? this.userState,
       cardsState: cardsState ?? this.userState,
+      currentUser: currentUser ?? this.currentUser
     );
   }
 
   @override
   int get hashCode =>
-  userState.hashCode ^ cardsState.hashCode;
+  userState.hashCode ^ cardsState.hashCode ^ currentUser.hashCode;
 
   @override
   bool operator ==(Object other) =>
       identical(this, other) ||
-          other is AppState && userState == other.userState || other is AppState && cardsState == other.cardsState;
+          other is AppState && userState == other.userState || other is AppState && cardsState == other.cardsState
+          || other is AppState && currentUser == other.currentUser;
+
+@override
+  String toString() {                                    
+    return 'AppState{currentUser: $currentUser}';
+  }
+
+
+
 }
+
+ 
